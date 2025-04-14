3492d0c054c3c3f303118a245cea9c5d8ec35ca4
JeongInu <itsiuj92@gmail.com>
2025-04-14 09:23:21 +0900

    Added gitignore after nodemon, ts-node

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-const hello = () => "hi";
+interface BlockShape{
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
+
+class Block implements BlockShape{
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ){}
+}
